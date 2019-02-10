cfe35f304c5ed9842a6526c1e6f006b9e07e09e3
Mimickal <mimickal.dev@gmail.com>
2019-02-10 18:18:03 -0500

    Test for MediaInfo

--- test/models/Models_test.js
+++ test/models/Models_test.js
@@ -3,6 +3,7 @@ const expect = require('chai').expect;
 const knex = require('../../app/lib/bookshelf').knex;
 const testdata = require('../../seed/test/data/test_data');
 
+const MediaInfo = require('../../app/models/MediaInfo');
 const Media = require('../../app/models/Media');
 
 describe('Models', function() {
@@ -15,7 +16,19 @@ describe('Models', function() {
 		return knex.seed.run();
 	});
 
+	describe('MediaInfo', function() {
+		it('Getting MedaiInfo record', async function() {
+			let gotModel = await MediaInfo
+				.where({id: testdata.mediaInfo[0].id})
+				.fetch();
+			// TODO validate date
+			let gotInfo = gotModel.toJSON();
+			expect(gotInfo).to.deep.equal(testdata.mediaInfo[0]);
+		});
+	});
+
 	describe('Media', function() {
+		// TODO add test validating date
 		it('Fetching associated MediaInfo', async function() {
 			let gotMedia = await Media
 				.where({id: testdata.media[0].id})
