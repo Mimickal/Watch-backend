56c8f3c8574ed37de968b9e7a9b60f27f20daa1f
Mimickal <mimickal.dev@gmail.com>
2019-04-25 22:10:02 -0400

    Some better media test data

--- seed/test/data/test_data.js
+++ seed/test/data/test_data.js
@@ -24,6 +24,52 @@ const media1 = new Media()
 	.withFile(new File())
 	.withFile(new File());
 
+const media2 = new Media()
+	.withTitle('Brüno', 'Bruno')
+	.withType(1)
+	.withRuntime(81)
+	.withAddDate(new Date(2019, 1, 1))
+	.withInfo(new Info()
+		.withImdb('tt0889583')
+		.withScore(5.8, 5.4)
+		.withReleaseDate(new Date(2009, 7, 10))
+		.withPlot(
+			'Flamboyant and gay Austrian Brüno looks for new fame in America.',
+			`Brüno is a gay Austrian fashion guru. He has his own fashion based
+			 television show, Funkyzeit, the most popular German-language show
+			 of its kind outside of Germany.`
+		)
+	);
+
+const media3 = new Media()
+	.withTitle('3:10 to Yuma', '3 10 to Yuma')
+	.withType(1)
+	.withRuntime(122)
+	.withAddDate(new Date(2019, 2, 1))
+	.withInfo(new Info()
+		.withImdb('tt0381849')
+		.withScore(7.7, null)
+		.withReleaseDate(new Date(2007, 9, 7))
+		.withPlot(`A small-time rancher agrees to hold a captured outlaw who's
+			awaiting a train to go to court in Yuma. A battle of wills ensues as
+			the outlaw tries to psych out the rancher.`
+		)
+	);
+
+const media4 = new Media()
+	.withTitle('300', null)
+	.withType(1)
+	.withRuntime(117)
+	.withAddDate(new Date(2019, 3, 1))
+	.withInfo(new Info()
+		.withImdb('tt0416449')
+		.withScore(7.7, null)
+		.withReleaseDate(new Date(2007, 3, 9))
+		.withPlot(`King Leonidas of Sparta and a force of 300 men fight the
+			Persians at Thermopylae in 480 B.C.`
+		)
+	);
+
 const series1 = new Series()
 	.withEndDate(Date.now())
 	.withMedia(new Media()
@@ -46,27 +92,171 @@ const series1 = new Series()
 		.withMedia(new Media())
 	);
 
+const series2 = new Series()
+	.withEndDate(new Date(2015, 6, 14))
+	.withMedia(new Media()
+		.withTitle('China, IL', 'China IL')
+		.withType(2)
+		.withRuntime(22)
+		.withAddDate(new Date(2019, 4, 1))
+		.withInfo(new Info()
+			.withImdb('tt2058221')
+			.withScore(6.6, null)
+			.withReleaseDate(new Date(2018, 5, 25))
+			.withPlot(`Steve and Frank Smith are brothers who teach the history
+				department in the worst university in America.`
+			)
+		)
+	);
+
+const series3 = new Series()
+	.withEndDate(new Date(2015, 8, 26))
+	.withMedia(new Media()
+		.withTitle('Aqua Teen Hunger Force', 'Aqua Teen Hunger Force')
+		.withType(2)
+		.withRuntime(15)
+		.withAddDate(new Date(2019, 5, 1))
+		.withInfo(new Info()
+			.withImdb('tt0297494')
+			.withReleaseDate(new Date(2000, 12, 30))
+			.withPlot(`The misadventures of a milkshake, an order of fries, a
+				meatball, and their retired next door neighbor in the suburbs of
+				New Jersey.`,
+				`The Aqua Teen Hunger Force debuted on episode 92 "Baffler Meal"
+				of the cartoon talk-show "Space Ghost Coast to Coast" According
+				to TVtome.com, Master Shake is portrayed as being a chocolate
+				milkshake in this episode, although he's a pistachio shake in
+				the series`
+			)
+		)
+	)
+	.withEpisode(new Episode()
+		.withNumber(1, 1)
+		.withMedia(new Media()
+			.withTitle('Rabbot', 'Rabbot')
+			.withType(3)
+			.withRuntime(12)
+			.withAddDate(new Date(2019, 5, 1))
+			.withInfo(new Info()
+				.withImdb('tt0821172')
+				.withScore(7.4, null)
+				.withPlot(`Dr. Weird invents a robot rabbit that escapes and
+					destroys Carls car. So now it's up to Aqua Teen Hunger Force
+					to find out who did it.`
+				)
+			)
+		)
+	)
+	.withEpisode(new Episode()
+		.withNumber(1, 3)
+		.withMedia(new Media()
+			.withTitle('Bus of the Undead', 'Bus of the Undead')
+			.withType(3)
+			.withRuntime(12)
+			.withAddDate(new Date(2019, 6, 1))
+			.withInfo(new Info()
+				.withImdb('tt0514061')
+				.withScore(8.3, null)
+				.withPlot(`Dr. Weird creates a mutant creature named
+					MothMonsterMan. The creature escapes and flies to the Aqua
+					Teens' house, attracted by the light.`
+				)
+			)
+		)
+	)
+	.withEpisode(new Episode()
+		.withNumber(2, 1)
+		.withMedia(new Media()
+			.withTitle('Super Birthday Snake', 'Super Birthday Snake')
+			.withType(3)
+			.withRuntime(12)
+			.withAddDate(new Date(2019, 7, 1))
+			.withInfo(new Info()
+				.withImdb('tt0763336')
+				.withScore(8.3, null)
+				.withPlot(`Meatwad is expecting the arrival of his new pet, a
+					bunny. A snake arrives instead, and Master Shake is
+					suspiciously unsurprised.`
+				)
+			)
+		)
+	)
+	.withEpisode(new Episode()
+		.withNumber(2, 2)
+		.withMedia(new Media()
+			.withTitle('Super Hero', 'Super Hero')
+			.withType(3)
+			.withRuntime(12)
+			.withAddDate(new Date(2019, 6, 1))
+			.withInfo(new Info()
+				.withImdb('tt0763337')
+				.withScore(8.1, null)
+				.withPlot(`Master Shake wants to become a super hero so he
+					steels three drums of highly toxic chemical waste and starts
+					to mutate.`
+				)
+			)
+		)
+	)
+	.withEpisode(new Episode()
+		.withNumber(2, 3)
+		.withMedia(new Media()
+			.withTitle('Super Bowl', 'Super Bowl')
+			.withType(3)
+			.withRuntime(12)
+			.withAddDate(new Date(2019, 2, 1))
+			.withInfo(new Info()
+				.withImdb('tt0815765')
+				.withScore(8.3, null)
+				.withPlot(`Meatwad wins two tickets two Super Bowl and both
+					Shake and Carl wants to go with him so they do whatever
+					Meatwad whats so he will choose them.`
+				)
+			)
+		)
+	);
+
 // Break nested objects out for seed functions
 module.exports = {
 	mediaInfo1: mediaInfo1,
 	media1: media1,
+	media2: media2,
+	media3: media3,
+	media4: media4,
 	series1: series1,
+	series2: series2,
+	series3: series3,
 
 	mediaInfo: [
 		mediaInfo1,
 		media1.info,
-		series1.media.info
+		media2.info,
+		media3.info,
+		media4.info,
+		series1.media.info,
+		series2.media.info,
+		series3.media.info,
+		...series3.episodes.map(episode => episode.media.info)
 	],
 	media: [
 		media1,
+		media2,
+		media3,
+		media4,
 		series1.media,
-		...series1.episodes.map(episode => episode.media)
+		series2.media,
+		series3.media,
+		...series1.episodes.map(episode => episode.media),
+		...series3.episodes.map(episode => episode.media)
 	],
 	series: [
-		series1
+		series1,
+		series2,
+		series3
 	],
 	episode: [
-		...series1.episodes
+		...series1.episodes,
+		...series3.episodes
 	],
 	file: [
 		...media1.files
